40d85cf67c696438ab96c8ecfcfa060cbd88851c
vishalag53 <vishal.agrawal1207@gmail.com>
2023-01-13 21:49:53 +0530

    Contact layout add

---
+++ app/src/main/java/com/vishalag53/contacts/ContactModel.java
@@ -0,0 +1,13 @@
+package com.vishalag53.contacts;
+
+public class ContactModel {
+    int img;
+    String name;
+    String number;
+
+    public ContactModel(int img,String name,String number){
+        this.img = img;
+        this.name = name;
+        this.number = number;
+    }
+}
